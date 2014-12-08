7f1228ca0733839e0a8b77656e8391edac865593
Dirk Louwers <dirk.louwers@stormlantern.nl>
2014-12-08 09:40:39 +0100

    * Fixed Formats vs Readers issue by using `lift` utility method

--- src/main/scala/net/ripe/whois/client/marshaling/WhoisResourceJsonProtocol.scala
+++ src/main/scala/net/ripe/whois/client/marshaling/WhoisResourceJsonProtocol.scala
@@ -30,18 +30,37 @@ trait WhoisResourceJsonProtocol extends DefaultJsonProtocol {
   }
 
   implicit val linkFormat = jsonFormat(Link, "xlink:type", "xlink:href")
+
   implicit val referenceFormat = jsonFormat(Reference, "link", "referenced-type")
-  implicit val attributeReader = new RootJsonReader[Attribute] {
-    override def read(json: JsValue): Attribute = {
-      json.withJSObject { obj =>
-        Attribute(
-          name = obj.readMandatoryFieldAs[String]("name"),
-          value = obj.readMandatoryFieldAs[String]("value"),
-          reference = obj.extractFromFields[Reference]("link", "referenced-type"),
-          comment = obj.readFieldAs[String]("comment")
-        )  
+
+  implicit val attributeFormat = lift {
+    new RootJsonReader[Attribute] {
+      override def read(json: JsValue): Attribute = {
+        json.withJSObject { obj =>
+          Attribute(
+            name = obj.readMandatoryFieldAs[String]("name"),
+            value = obj.readMandatoryFieldAs[String]("value"),
+            reference = obj.extractFromFields[Reference]("link", "referenced-type"),
+            comment = obj.readFieldAs[String]("comment")
+          )
+        }
       }
     }
   }
 
+  implicit val objectFormat = lift {
+    new RootJsonReader[Object] {
+      override def read(json: JsValue): Object = {
+        json.withJSObject { obj =>
+          Object(
+            objectType = obj.readMandatoryFieldAs[String]("type"),
+            link = obj.readMandatoryFieldAs[Link]("link"),
+            source = obj.fields.get("source").flatMap(_.asJsObject.readFieldAs[String]("id")).getOrElse(deserializationError("Source is missing")),
+            primaryKey = obj.readMandatoryFieldAs[Attribute]("primary-key"),
+            attributes = obj.fields.get("attributes").flatMap(_.asJsObject.readFieldAs[Set[Attribute]]("attribute")).getOrElse(deserializationError("Attributes are missing"))
+          )
+        }
+      }
+    }
+  }
 }
