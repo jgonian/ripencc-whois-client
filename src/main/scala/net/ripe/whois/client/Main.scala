9c2e9c7815bf56ecfb681926e7537046269f6891
Yannis Gonianakis <jgonian@gmail.com>
2014-12-19 15:01:03 +0100

    Cleanup main.

--- src/main/scala/net/ripe/whois/client/Main.scala
+++ src/main/scala/net/ripe/whois/client/Main.scala
@@ -1,8 +1,11 @@
 package net.ripe.whois.client
 
 import akka.actor.ActorSystem
+import akka.dispatch.Futures
 import akka.event.Logging
+import net.ripe.whois.client.view.WhoisResponse
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 
@@ -13,18 +16,28 @@ object Main extends App {
   val log = Logging(system, getClass)
   log.info("starting up...")
 
-  val whoisBaseUrl = "https://rest-test.db.ripe.net/test"
+  val toRpslString: PartialFunction[WhoisResponse, WhoisResponse] = {
+    case response =>
+      val whoisObject = response.objects.head
+      println(whoisObject.toRpslString)
+      println(s"is ${whoisObject.primaryKey} authenticated? ${response.objects.head.isAuthenticated}")
+      println()
+      response
+  }
 
-  val whoisClient = new HttpWhoisClient(whoisBaseUrl)
-  val authentications = Seq(WhoisCrowdAuthentication("7Okn8y5Vyg8kOwY0tP4yqw00"), WhoisPasswordAuthentication("emptypassword"))
 
-  whoisClient.lookup("mntner", "TEST-DBM-MNT", authentications).onComplete {
-    case Success(response) =>
-      log.info("shutting down...SUCCESS" + response.objects.head.attributes)
-      system.shutdown()
-    case Failure(ex) =>
-      log.info(ex.getMessage)
-      log.info("shutting down...FAILURE")
+  val whoisClient = new HttpWhoisClient("https://rest-test.db.ripe.net/test")
+
+  val lookup1 = whoisClient.lookup("mntner", "TEST-DBM-MNT")
+  val lookup2 = whoisClient.lookup("mntner", "TEST-MNT")
+
+  lookup1 onSuccess { toRpslString }
+  lookup2 onSuccess { toRpslString }
+
+
+  Future.sequence(Seq(lookup1, lookup2)).onComplete {
+    case _ =>
+      log.info("shutting down...")
       system.shutdown()
   }
 
